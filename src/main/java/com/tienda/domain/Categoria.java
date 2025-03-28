267df7c4c7351588b5371df461fc598aff0581fc
JoshuaGuerrero10 <158859278+JoshuaGuerrero10@users.noreply.github.com>
2025-03-27 18:20:37 -0600

    implementacion crud

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -14,23 +14,23 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name="categoria")
-public class Categoria implements Serializable 
-        private static final long serialVersionUID = 1L;
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name="id_categoria")
-        private Long idCategoria;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_categoria")
+    private Long idCategoria;
 
-        private String descripcion;
-        private String rutaImagen;
-        private boolean activo;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
 
-
-public Categoria() {
+    public Categoria() {
     }
 
-    public Categoria(String categoria, boolean activo) {
-        this.descripcion = categoria;
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
         this.activo = activo;
     }
+}
